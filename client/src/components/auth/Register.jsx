0904d9d29c5f57d30ffd3d97e750fba34e8a8019
wlockiv <walker.b.lockard@gmail.com>
2019-07-23 21:08:30 -0500

    finished up form appearance

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,6 +1,7 @@
+import { Button, Grid, Typography, Paper, TextField } from '@material-ui/core';
+import { KeyboardArrowLeft } from '@material-ui/icons';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Typography } from '@material-ui/core';
 
 class Register extends Component {
   constructor() {
@@ -38,12 +39,87 @@ class Register extends Component {
     const { errors } = this.state;
 
     return (
-      <Grid container style={{ maxWidth: '95%', margin: '0 auto' }} spacing={3}>
-        <Grid item sm={12}>
-          <Typography variant="h4">
+      <Grid
+        container
+        justify="center"
+        style={{ maxWidth: '95%', margin: '24px auto' }}
+        spacing={3}
+      >
+        <Grid item sm={2}>
+          <Button
+            style={{ lineHeight: 'normal' }}
+            variant="outlined"
+            component={Link}
+            to="/"
+          >
+            <KeyboardArrowLeft style={{ marginRight: 4 }} />
+            <span style={{ marginRight: 4 }}>Go Back Home</span>
+          </Button>
+        </Grid>
+        <Grid item sm={10}>
+          <Typography variant="h4" style={{ textAlign: 'right' }}>
             <b>Register</b> Below
           </Typography>
         </Grid>
+        <Paper
+          style={{
+            width: '50%',
+            minWidth: 300,
+            maxWidth: 500,
+            minHeight: 200,
+            padding: '1.5rem'
+          }}
+        >
+          <form action="">
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <TextField
+                  label="Name"
+                  name="name"
+                  style={{ width: '100%' }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Email"
+                  name="email"
+                  style={{ width: '100%' }}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Password"
+                  name="password"
+                  style={{ width: '100%' }}
+                  variant="outlined"
+                  type="password"
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Confirm Password"
+                  name="password2"
+                  style={{ width: '100%' }}
+                  variant="outlined"
+                  type="password"
+                  autoComplete="off"
+                />
+              </Grid>
+              <Grid item alignItems="center">
+                <Button
+                  style={{ width: '100%' }}
+                  variant="outlined"
+                  color="primary"
+                >
+                  Sign Up
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
       </Grid>
     );
   }
